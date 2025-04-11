a6fcb7ced4d4a904ad4016ee133e8688fd894181
Muhammad Talha <talha18513@gmail.com>
2025-04-11 10:00:37 +0500

    complete

--- Docs/Javascripts/footer.js
+++ Docs/Javascripts/footer.js
@@ -1,41 +1,74 @@
 const footer = document.getElementById("footer");
 
 footer.innerHTML = `
-<footer class="w-full mt-16 md:mt-28">
-            <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-                <!--Grid-->
-                <div
-                    class="grid grid-cols-1 sm:grid-cols-4 lg:grid-cols-5 md:gap-8 md:py-10  py-5 max-sm:max-w-sm max-sm:mx-auto ">
-                    <div class="mb-10 lg:col-span-2 lg:mb-0">
-                        <a href="./index.html" class="flex justify-center lg:justify-start">
-                            <img src="./Docs/Images/logo.png" class="h-20" alt="logo">
-                        </a>
-                        <p class="pt-8 text-sm text-gray-800 lg:max-w-xs text-center lg:text-left"> Excellence in
-                            Leather Tanning Since 1952</p>
-                    </div>
-                    <!--End Col-->
-                    <!--End Col-->
-                    <div class="lg:mx-auto text-left ">
-                        <h4 class="text-lg text-[#000] font-medium mb-5">Links</h4>
-                        <ul class="text-sm  transition-all duration-500">
-                            <li class="mb-4"><a href="./index.html" class="text-gray-600 hover:underline">Home</a></li>
-                            <li class="mb-4"><a href="./about-us.html" class="text-gray-600 hover:underline">About
-                                    Us</a></li>
-                            <li class="mb-4"><a href="./products.html"
-                                    class="text-gray-600 hover:underline">Products</a></li>
-                            <li class="mb-4"><a href="./contactUs.html"
-                                    class="text-gray-600 hover:underline">Contact Us</a></li>
-                        </ul>
-                    </div>
+    <footer class="max-w-[1485px] md:mt-20 mt-10 w-full mx-auto md:rounded-3xl bg-[#0c1524] md:p-10 p-5 pt-12 md:mb-8">
 
-                </div>
-                <!--Grid-->
-                <div class="py-7 border-t border-gray-200">
-                    <div class="flex items-center justify-center">
-                        <span class="text-sm text-gray-400 ">©<a href="./index.html">AIT Industries</a> 2025, All rights
-                            reserved.</span>
+        <div class="flex md:items-center justify-between md:flex-row flex-col gap-5">
+            <a href="./index.html">
+                <img src="./Docs/Assets/footer-logo.svg" class="h-12" alt="">
+            </a>
+            <h2 class="text-white md:text-4xl text-2xl font-bold">Trusted by Businesses
+                Nationwide</h2>
+        </div>
+
+        <div class="flex md:mt-20 mt-10 md:items-center justify-between md:flex-row flex-col gap-5">
+            <div class="max-w-sm w-full">
+                <h2 class="text-white md:text-2xl tracking-wide text-xl font-bold">Stay Updated</h2>
+                <p class="text-white mt-1 opacity-80">Sign up
+                    for our newsletter to
+                    receive expert insights,
+                    industry updates, and
+                    exclusive offers.</p>
+                <form action="" class="mt-4">
+                    <input type="text" placeholder="Enter Mail"
+                        class="w-full border-b border-[#3a414d] text-white py-3 bg-transparent">
+                    <div class="flex mt-4">
+                        <a href=""
+                            class="bg-[#ff8159] text-white group px-4 font-semibold flex overflow-hidden items-center gap-2 rounded-lg py-2.5 transition-all duration-500 relative">
+                            <span
+                                class="bg-[#fff] w-0 h-full group-hover:w-full transition-all duration-500 absolute top-0 left-0"></span>
+                            <span class="z-10 group-hover:text-[#ff8159] transition-all duration-500">
+                                Subscribe No
+                            </span>
+                            <span
+                                class="text-[#ff8159] w-[32px] h-[32px] flex group-hover:bg-[#ff8159] group-hover:text-[#fff] transition-all duration-500 items-center justify-center z-10 bg-white rounded-lg">
+                                <i class="fa-solid fa-arrow-right -rotate-45"></i>
+                            </span>
+                        </a>
                     </div>
-                </div>
+                </form>
             </div>
-        </footer>
+            <div class="flex md:flex-row flex-col flex-wrap md:gap-20 gap-8 md:mt-0 mt-6">
+
+                <div class="">
+                    <h2 class="text-white md:text-xl tracking-wide text-xl font-semibold">Support </h2>
+                    <ul class="md:mt-6 mt-4 text-white opacity-80 space-y-2">
+                        <li><a class="hover:text-[#ff8159] transition-all duration-500" href="">Book appointment</a></li>
+                        <li><a class="hover:text-[#ff8159] transition-all duration-500" href="">Get help </a></li>
+                        <li><a class="hover:text-[#ff8159] transition-all duration-500" href="">Text us</a></li>
+                    </ul>
+                </div>
+                <div class="">
+                    <h2 class="text-white md:text-xl tracking-wide text-xl font-semibold">Follow Us </h2>
+                    <ul class="mt-6 text-white opacity-80 flex gap-2">
+                        <li><a class="hover:text-[#ff8159] hover:bg-white transition-all text-white w-10 h-10 flex items-center justify-center rounded-full bg-[#ff8159]  duration-500" href="">
+                            <i class="fa-brands fa-facebook-f"></i>
+                        </a></li>
+                        <li><a class="hover:text-[#ff8159] hover:bg-white transition-all text-white w-10 h-10 flex items-center justify-center rounded-full bg-[#ff8159]  duration-500" href="">
+                            <i class="fa-brands fa-instagram text-lg"></i>
+                        </a></li>
+                        <li><a class="hover:text-[#ff8159] hover:bg-white transition-all text-white w-10 h-10 flex items-center justify-center rounded-full bg-[#ff8159]  duration-500" href="">
+                            <i class="fa-brands fa-linkedin-in"></i>
+                        </a></li>
+                    </ul>
+                </div>
+                
+            </div>
+        </div>
+
+        <div class="border-t pt-5 text-center border-[#3a414d] mt-14">
+            <p class="md:text-lg text-white">copyright © 2024 wexico. all rights reserved.</p>
+        </div>
+
+    </footer>
 `
--- Docs/Javascripts/navbar.js
+++ Docs/Javascripts/navbar.js
@@ -1,43 +1,61 @@
 const navbar = document.getElementById("navbar");
 
 navbar.innerHTML = `
-         <nav>
-            <div class="max-w-7xl mx-auto flex justify-between items-center w-full p-5">
-                <a href="./index.html" class="flex justify-center lg:justify-start">
-                    <img src="./Docs/Images/logo.png" class="h-14" alt="logo">
-                </a>
-                <ul class="md:flex hidden items-center gap-7">
-                    <li><a href="./index.html" class="hover:text-[#6c2313] transition-all duration-500">Home</a></li>
-                    <li><a href="./about-us.html" class="hover:text-[#6c2313] transition-all duration-500">About Us</a>
-                    </li>
-                    <li><a href="./products.html" class="hover:text-[#6c2313] transition-all duration-500">Products</a>
-                    </li>
-                    <li><a href="./contactUs.html" class="hover:text-[#6c2313] transition-all duration-500">Contact Us</a>
-                    </li>
-                </ul>
-                <i id="mobileMenuBtn" class="fa-solid fa-bars md:hidden text-2xl cursor-pointer"></i>
-            </div>
-        </nav>
-
-        <div id="mobileMenuBg"
-            class="fixed cursor-crosshair transition-all duration-500 top-0 right-0 w-0 h-full bg-[#0d0d0c] opacity-50 z-40">
-        </div>
-        <div id="mobileMenu"
-            class="fixed right-0 top-0 bg-white max-w-[30rem] w-0 overflow-hidden h-screen z-50 transition-all duration-500">
-            <i id="mobileMenuCloseBtn"
-                class="fa-solid fa-x z-50 relative flex items-end justify-end p-5 text-2xl cursor-pointer"></i>
-            <ul class="flex relative z-50 flex-col p-10 gap-7">
-                <li><a href="./index.html" class="hover:text-[#6c2313] transition-all font-medium duration-500">Home</a>
+     <nav>
+        <div class="max-w-7xl mx-auto flex justify-between items-center w-full p-5 md:py-8">
+            <a href="./index.html" class="flex justify-center lg:justify-start">
+                <img src="./Docs/Assets/logo.svg" class="h-[39px]" alt="logo">
+            </a>
+            <ul class="md:flex hidden items-center gap-10">
+                <li><a href="./index.html" class="hover:text-[#ff8159] transition-all font-medium duration-500">Home</a>
                 </li>
-                <li><a href="./about-us.html"
-                        class="hover:text-[#6c2313] transition-all font-medium duration-500">About Us</a></li>
-                <li><a href="./products.html"
-                        class="hover:text-[#6c2313] transition-all font-medium duration-500">Products</a>
-                <li><a href="./contactUs.html"
-                        class="hover:text-[#6c2313] transition-all font-medium duration-500">Contact Us</a>
+                <li><a href="./about.html"
+                        class="hover:text-[#ff8159] transition-all font-medium duration-500">About</a></li>
+                <li><a href="./services.html"
+                        class="hover:text-[#ff8159] transition-all font-medium duration-500">Services</a></li>
+                <li><a href="./blog.html" class="hover:text-[#ff8159] transition-all font-medium duration-500">Blog</a>
                 </li>
+                <li><a href="./contact.html"
+                        class="hover:text-[#ff8159] transition-all font-medium duration-500">Contact</a></li>
             </ul>
+            <a href=""
+                class="bg-[#ff8159] text-white group px-4 font-semibold md:flex hidden overflow-hidden items-center gap-2 rounded-lg py-2.5 transition-all duration-500 relative">
+                <span
+                    class="bg-[#0c1524] w-0 h-full group-hover:w-full transition-all duration-500 absolute top-0 left-0"></span>
+                <span class="z-10">
+                    Book Now
+                </span>
+                <span
+                    class="text-[#ff8159] w-[32px] h-[32px] flex items-center justify-center z-10 bg-white rounded-lg">
+                    <i class="fa-solid fa-arrow-right -rotate-45"></i>
+                </span>
+            </a>
+
+            <i id="mobileMenuBtn" class="fa-solid fa-bars md:hidden text-2xl cursor-pointer"></i>
         </div>
+    </nav>
+
+    <div id="mobileMenuBg"
+        class="fixed cursor-crosshair transition-all duration-500 top-0 right-0 w-0 h-full bg-[#0d0d0c] opacity-50 z-40">
+    </div>
+    <div id="mobileMenu"
+        class="fixed right-0 top-0 bg-white max-w-[30rem] w-0 overflow-hidden h-screen z-50 transition-all duration-500">
+        <i id="mobileMenuCloseBtn"
+            class="fa-solid fa-x z-50 relative flex items-end justify-end p-5 text-2xl cursor-pointer"></i>
+        <ul class="flex relative z-50 flex-col p-10 gap-7">
+            <li><a href="./index.html" class="hover:text-[#6c2313] transition-all font-medium duration-500">Home</a>
+            </li>
+            <li><a href="./about-us.html" class="hover:text-[#6c2313] transition-all font-medium duration-500">About
+                    </a></li>
+            <li><a href="./services.html"
+                    class="hover:text-[#6c2313] transition-all font-medium duration-500">Services</a>
+            <li><a href="./blog.html" class="hover:text-[#6c2313] transition-all font-medium duration-500">Blog
+                    </a>
+            <li><a href="./contact.html" class="hover:text-[#6c2313] transition-all font-medium duration-500">Contact
+                    </a>
+            </li>
+        </ul>
+    </div>
 `
 
 
